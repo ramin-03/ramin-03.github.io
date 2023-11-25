8b84a4cc25f93d5a38676b9b25e6b4a67971f158
ramin-03 <rohmmy03@gmail.com>
2023-11-24 22:46:35 -0800

    Fixed out file idex

---
+++ docs/_next/static/25B6C5cr_Z4kszfsEPcGi/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b231492a58f755c4.css","static/chunks/pages/index-4f8ddb16557ea66b.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  distDir: "docs"
 }
 
 module.exports = nextConfig
