b43a15d0a5b5f50f35cf53051fe6b6649aea90ef
ramin-03 <rohmmy03@gmail.com>
2023-12-02 23:28:55 -0800

    Updating out file

---
+++ docs/_next/static/JltPBpQzng38dKBUG9p-L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/0d11bc2528c9caa1.css","static/chunks/pages/index-5d75677353d3b654.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
